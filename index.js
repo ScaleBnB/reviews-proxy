6d59f6bcb4c6b2675fb34ba280c3ee14aa767c12
Hsin tung <hsintung@Hsins-MacBook-Pro.local>
2020-02-24 22:48:14 -0800

    refactor to proxy server

--- index.js
+++ index.js
@@ -10,51 +10,105 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.use(express.static('public')); // for serving static files
 
 // Calendar Proxy Response
-app.get('/month', (req, res) => {
-    axios.get('http://3.16.164.208:3001', {
-      params: req.query
-    })
-    .then(({ data }) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  });
+// app.get('/month', (req, res) => {
+//     axios.get('http://3.16.164.208:3001', {
+//       params: req.query
+//     })
+//     .then(({ data }) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+//   });
   
   // Image Carousel Proxy Response
-  app.get('/gallery/:id', (req, res) => {
-    var id = req.query.id;
-    axios.get(`http://13.57.225.193:3000/gallery/${id}`)
-    .then(({ data }) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  });
+  // app.get('/gallery/:id', (req, res) => {
+  //   var id = req.query.id;
+  //   axios.get(`http://13.57.225.193:3000/gallery/${id}`)
+  //   .then(({ data }) => {
+  //     res.json(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  // });
   
   // Reviews Proxy Response
-  app.get('/api/0', (req, res) => {
-    axios.get(`http://13.57.226.2:3003/:3003/api/0`)
-    .then(({ data }) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  });
+  // app.get('/api/0', (req, res) => {
+  //   axios.get(`http://13.57.226.2:3003/:3003/api/0`)
+  //   .then(({ data }) => {
+  //     res.json(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  // });
   
   // Recommendations Proxy Response
-  app.get('/listings', (req, res) => {
-    axios.get(`http://52.53.211.102:3002/`)
-    .then(({ data }) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // app.get('/listings', (req, res) => {
+  //   axios.get(`http://52.53.211.102:3002/`)
+  //   .then(({ data }) => {
+  //     res.json(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  // });
+
+// Create / POST - create a new item
+app.post(`/properties/:propertyID/reviews`, (req, res) => {
+  axios.post(`http://localhost:3003/properties/${req.params.propertyID}/reviews`, req.body)
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
   });
+});
+// Read / GET - get all reviews of a given property
+app.get(`/properties/:propertyID/reviews`, (req, res) => {
+  axios.get(`http://localhost:3003/properties/${req.params.propertyID}/reviews`)
+  .then((response) => {
+    res.json(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+// Delete / DELETE - delete a review of a given property
+app.delete('/properties/:propertyID/reviews', (req, res) => {
+  axios.delete(`http://localhost:3003/properties/${req.params.propertyID}/reviews/?reviewID=${req.query.reviewID}`)
+  .then((response) => {
+    res.json(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+// Put / DELETE - delete a host response
+app.put('/properties/:propertyID/replies', (req, res) => {
+  axios.put(`http://localhost:3003/properties/${req.params.propertyID}/replies/?reviewID=${req.query.reviewID}`, req.body)
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+// Update / PUT - update an item based on query
+app.put('/properties/:propertyID/reviews', (req, res) => {
+  axios.put(`http://localhost:3003/properties/${req.params.propertyID}/reviews?reviewID=${req.query.reviewID}`, req.body)
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
   
 
 app.listen(port, () => {
